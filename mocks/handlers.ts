5c5c5527c1451d6ea3f3de2787b35acae808448c
sumin.kang <magic687217@gmail.com>
2026-08-27 16:27:56 +0900

    test: 참여자 조회 mock 핸들러 추가 (#38)

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -2,10 +2,12 @@ import { authHandlers } from "./authHandlers";
 import { courseHandlers } from "./courseHandlers";
 import { placeHandlers } from "./placeHandlers";
 import { preferenceHandlers } from "./preferenceHandlers";
+import { participantHandlers } from "./participantHandlers";
 
 export const handlers = [
   ...preferenceHandlers,
   ...courseHandlers,
   ...authHandlers,
   ...placeHandlers,
+  ...participantHandlers,
 ];
